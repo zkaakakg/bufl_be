f5e467a531e63d3c23509d19f5fa1311f6c106b9
zkaakakag <cscom222@naver.com>
2025-03-11 17:10:24 +0900

    bcryptjs -> bcrypt

--- api/users.js
+++ api/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const session = require("express-session");
 const router = express.Router();
 const db = require("../db/db");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const { v4: uuidv4 } = require("uuid");
 
 /**
