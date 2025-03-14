94e8c8a0938992b9a6f043a5b7d764cd7a731684
ju2hong <juihong730@gmail.com>
2025-03-14 14:05:17 +0900

    스웨거 문구 변경

--- swagger/swagger-ai-goal.js
+++ swagger/swagger-ai-goal.js
@@ -63,7 +63,7 @@
  *   /api/ai-goals/generate-goals:
  *     post:
  *       tags: [Ai]
- *       summary: 사용자가 선택한 목표를 생성하고 자동이체를 실행합니다.
+ *       summary: 사용자가 선택한 목표를 생성 합니다.
  *       description: 사용자가 선택한 목표 정보를 기반으로 목표를 생성.
  *       requestBody:
  *         required: true
