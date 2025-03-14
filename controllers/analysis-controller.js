0de26de463f7713d6390f74d8b3eee512678677d
zkaakakag <cscom222@naver.com>
2025-03-14 09:42:23 +0900

    test

--- controllers/analysis-controller.js
+++ controllers/analysis-controller.js
@@ -3,6 +3,8 @@ const consumptionService = require("../services/consumption-service");
 exports.getConsumptionPattern = async (req, res) => {
   try {
     const sessionId = req.cookies.sessionId;
+    console.log(sessionId);
+
     if (!sessionId) return res.status(401).json({ message: "세션 없음" });
 
     const result = await consumptionService.analyzeConsumptionPattern(
